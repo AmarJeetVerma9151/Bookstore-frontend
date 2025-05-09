59f6c1bb065fd269b631f7891f54344ff7edbcfc
AmarJeetVerma9151 <vermaamarjeet095@gmail.com>
2025-05-10 00:47:17 +0530

    add

--- src/courses/Courses.jsx
+++ src/courses/Courses.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import Navbar from '../Components/Navbar'
 import Course from '../Components/Course'
 import Footer from '../Components/Footer'
