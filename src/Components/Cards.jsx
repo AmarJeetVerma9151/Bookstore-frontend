3b997ea998c61c1def72f17e3f818cbc21b3ad00
AmarJeetVerma9151 <vermaamarjeet095@gmail.com>
2025-05-10 13:25:20 +0530

    add

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -9,7 +9,7 @@ function Cards({ item }) {
 
     const useData=useSelector((state) => state.user)
        const user = useData.user?.role;
-        console.log(user)
+        
     try {
       const res = await axios.delete(`https://bookstore-backend-ru0v.onrender.com/book/deletebook/${Id}`);
       const data = res.data;
@@ -60,12 +60,12 @@ function Cards({ item }) {
               <button className="px-4 py-1 text-sm text-white bg-pink-500 rounded-full hover:bg-pink-400 transition">
                 Buy Now
               </button>
-              <button
+              {user=="admin"&&<button
                 onClick={() => handleDelete(item._id)}
                 className="px-4 py-1 text-sm text-white bg-red-600 rounded-full hover:bg-red-400 transition"
               >
                 Delete
-              </button>            </div>
+              </button>            }</div>
           </div>
         </div>
       </motion.div>
